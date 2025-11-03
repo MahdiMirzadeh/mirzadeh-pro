3d7dee7db6571e9dbf6791938ad27f57491f174e
MahdiMirzadeh <mahdi@mirzadeh.pro>
2025-11-03 11:22:01 +0330

    wrangler: Skip http->https redirect if the ip is 127.0.0.1 (aka running in local dev using the 'wrangler dev' command)

--- .worker.js
+++ .worker.js
@@ -3,9 +3,15 @@ export default {
     const url = new URL(request.url);
 
     // Redirect http -> https
-    if (url.protocol === "http:") {
-      url.protocol = "https:";
-      return Response.redirect(url.toString(), 301);
+    const cfIp = request.headers.get('cf-connecting-ip');
+    const isDevEnvironment = cfIp === '127.0.0.1' || cfIp === '::1';
+    if (url.protocol === 'http:' && !isDevEnvironment) {
+      return new Response(null, {
+        status: 301,
+        headers: {
+          'Location': `https://${url.host}${url.pathname}${url.search}`
+        }
+      });
     }
 
     // Serve static assets
